5b2d398ac03b46676ed9a6b73bb5b7444bc9c27f
Meghan Gunderson <megundersonmn@gmail.com>
2020-02-01 19:17:06 -0600

    movies rendering to DOM!

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -17,20 +17,26 @@ class Home extends Component {
         return (
             <div>
                 <h1>Home Page</h1>
-                {JSON.stringify(this.props.reduxStore.movies)}
-                {this.props.reduxStore.movies}
+                {/* {JSON.stringify(this.props.reduxStore.movies)} */}
+                {this.props.reduxStore.movies.map((movie) => {
+                    return <div key={movie.id}>
+                        <img src={movie.poster}></img>
+                        <p>{movie.title}</p>
+                        <p>{movie.description}</p>
+                        </div>
+                    })}
             </div>
         )
-    }
+                }
 
 
-}
-
+                }
+                
 const mapStateToProps = (reduxStore) => {
     return (
         {
-            reduxStore
-        }
-    )
-}
+                    reduxStore
+                }
+                )
+            }
 export default connect(mapStateToProps)(Home)
